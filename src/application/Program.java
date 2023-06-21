998ecd2e2bf683ee264977da4f5e28e6b2e47695
Leonidas Marques <leonidasmmarques@outlook.com>
2023-06-21 19:55:29 -0300

    implementando classe inicial

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,31 @@ package application;
 
 import java.util.Scanner;
 
+import entities.Aluno;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		Aluno alun = new Aluno();
+		
+		System.out.println("Escreva o nome do aluno: ");
+		alun.nome = sc.nextLine();
+		System.out.println("Informe a nota do primeiro trimestre: ");
+		alun.nota1 = sc.nextDouble();
+		System.out.println("Informe a nota do primeiro trimestre: ");
+		alun.nota2 = sc.nextDouble();
+		System.out.println("Informe a nota do primeiro trimestre: ");
+		alun.nota3 = sc.nextDouble();
+		
+		double notaTotal = alun.somaNota();
 		
 		
+		String resultado = alun.validaAprovacao();
+		//System.out.println(resultado);
+		
+		System.out.println(alun);
 
 	}
 
--- src/entities/Aluno.java
+++ src/entities/Aluno.java
@@ -7,5 +7,26 @@ public class Aluno {
 	public double nota2;
 	public double nota3;
 	
+	public double somaNota() {
+		return nota1 + nota2 + nota3;
+	}
+	
+	public String validaAprovacao() {
+		double aux; 
+		if(somaNota() > 60) {
+			return "PASS";
+		}else {
+			aux = 60 - somaNota();
+			return "RECUPERAÇÃO /nl FALTA" + aux + "PONTOS";
+		}		
+	}
+	
+	@Override
+	public String toString() {
+		return "RESULTADO FINAL =" + somaNota() + "\nl  " + validaAprovacao();
+	}
+
+
+	
 	
 }
